fca7684d3086e5d0cc138ba45448eeac4038db8a
amandadetofol <amandadetofol@MacBook-Pro-de-Amanda.local>
2024-10-28 15:50:49 -0300

    creating test for fetch secure storage

--- flutter_course/lib/lib/data/usecases/save_secure_cache_storage/save_current_cache_storege.dart
+++ flutter_course/lib/lib/data/usecases/save_secure_cache_storage/save_current_cache_storege.dart
@@ -2,7 +2,8 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../../cache/cache.dart';
 
-class LocalStorageAdapter implements SaveSecureCacheStorage {
+class LocalStorageAdapter
+    implements SaveSecureCacheStorage, FetchSecureCacheStorage {
   final FlutterSecureStorage flutterSecureStorage;
 
   LocalStorageAdapter({required this.flutterSecureStorage});
@@ -16,5 +17,15 @@ class LocalStorageAdapter implements SaveSecureCacheStorage {
       key: key,
       value: value,
     );
+  }
+
+  @override
+  Future<String> fetchSecure(String key) async {
+    final response = await flutterSecureStorage.read(key: key);
+
+    if (response?.isNotEmpty ?? false) {
+      return response!;
+    }
+    return 'error';
   }
 }
--- flutter_course/test/infra/cache/local_storage_adapter_test.dart
+++ flutter_course/test/infra/cache/local_storage_adapter_test.dart
@@ -1,5 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_course/lib/data/usecases/usecases.dart';
+import 'package:flutter_course/lib/domain/helpers/helpers.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -12,47 +13,98 @@ void main() {
   late String value;
   late LocalStorageAdapter sut;
 
+  mock() {
+    when(() => flutterSecureStorageSpy.write(
+          key: any(named: 'key'),
+          value: any(named: 'value'),
+        )).thenAnswer((_) async => Future.value());
+  }
+
+  mockError() {
+    when(() => flutterSecureStorageSpy.write(
+          key: any(named: 'key'),
+          value: any(named: 'value'),
+        )).thenThrow(Exception());
+  }
+
   setUp(() {
     flutterSecureStorageSpy = FlutterSecureStorageSpy();
     key = faker.lorem.word();
     value = faker.guid.guid();
 
     sut = LocalStorageAdapter(flutterSecureStorage: flutterSecureStorageSpy);
+
+    mock();
   });
 
-  test('Should call save secure with correct values', () async {
-    when(() => flutterSecureStorageSpy.write(
-          key: any(named: 'key'),
-          value: any(named: 'value'),
-        )).thenAnswer((_) async => Future.value());
+  group('savesecure', () {
+    test('Should call save secure with correct values', () async {
+      await sut.saveSecure(
+        key,
+        value,
+      );
 
-    await sut.saveSecure(
-      key,
-      value,
-    );
+      verify(
+        () => flutterSecureStorageSpy.write(
+          key: key,
+          value: value,
+        ),
+      ).called(1);
+    });
 
-    verify(
-      () => flutterSecureStorageSpy.write(
-        key: key,
-        value: value,
-      ),
-    ).called(1);
+    test('Should throw if save secure throws', () async {
+      mockError();
+      final response = sut.saveSecure(
+        key,
+        value,
+      );
+
+      expect(
+        response,
+        throwsA(const TypeMatcher<Exception>()),
+      );
+    });
   });
 
-  test('Should throw if save secure throws', () async {
-    when(() => flutterSecureStorageSpy.write(
-          key: any(named: 'key'),
-          value: any(named: 'value'),
-        )).thenThrow(Exception());
+  group('fetchsecure', () {
+    mockFetch() {
+      when(() => flutterSecureStorageSpy.read(
+            key: any(named: 'key'),
+          )).thenAnswer((_) async => key);
+    }
 
-    final response = sut.saveSecure(
-      key,
-      value,
-    );
+    mockFetchSecureError() {
+      when(() => flutterSecureStorageSpy.read(
+            key: any(named: 'key'),
+          )).thenThrow(DomainError.unexpected);
+    }
 
-    expect(
-      response,
-      throwsA(const TypeMatcher<Exception>()),
-    );
+    test('Should call fetch secure with correct values', () async {
+      mockFetch();
+
+      await sut.fetchSecure(key);
+
+      verify(
+        () => flutterSecureStorageSpy.read(
+          key: key,
+        ),
+      ).called(1);
+    });
+
+    test('Should return correct value', () async {
+      mockFetch();
+
+      final response = await sut.fetchSecure(key);
+
+      expect(response, key);
+    });
+
+    test('Shoult throw Unexpected Error if Secure Storage throws', () async {
+      mockFetchSecureError();
+
+      final response = sut.fetchSecure(key);
+
+      expect(response, throwsA(DomainError.unexpected));
+    });
   });
 }
