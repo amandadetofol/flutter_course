0cc72815c576a06dee6392eb6bc9c2c1fe8e096a
amandadetofol <amandadetofol@MacBook-Pro-de-Amanda.local>
2024-11-01 08:13:36 -0300

    test ensure test page presents spinner on page load

---
+++ flutter_course/test/ui/pages/splash_page_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+
+void main() {
+  Future<void> loadPage(WidgetTester tester) async {
+    await tester.pumpWidget(
+      GetMaterialApp(
+        initialRoute: '/',
+        getPages: [
+          GetPage(
+            name: '/',
+            page: () => const SplashPage(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  testWidgets(
+    'Should present spinner on page load',
+    (tester) async {
+      await loadPage(tester);
+
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    },
+  );
+}
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('4Dev'),
+      ),
+      body: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
