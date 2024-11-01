5a56357efa3765c4aea7649f442a0c60d0af7e0c
amandadetofol <amandadetofol@MacBook-Pro-de-Amanda.local>
2024-11-01 08:28:05 -0300

    test ensure test page calls load current accouint

--- flutter_course/test/ui/pages/splash_page_test.dart
+++ flutter_course/test/ui/pages/splash_page_test.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:mocktail/mocktail.dart';
+
+class SplashPresenterSpy extends Mock implements SplashPresenter {}
 
 void main() {
+  late SplashPresenterSpy presenter;
+
+  void mockPresenter() {
+    when(() => presenter.loadCurrentAccount()).thenAnswer((_) async {});
+  }
+
   Future<void> loadPage(WidgetTester tester) async {
     await tester.pumpWidget(
       GetMaterialApp(
@@ -10,12 +19,16 @@ void main() {
         getPages: [
           GetPage(
             name: '/',
-            page: () => const SplashPage(),
+            page: () => SplashPage(presenter: presenter),
           ),
         ],
       ),
     );
   }
+
+  setUpAll(() {
+    presenter = SplashPresenterSpy();
+  });
 
   testWidgets(
     'Should present spinner on page load',
@@ -25,10 +38,42 @@ void main() {
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
     },
   );
+
+  testWidgets(
+    'Should call load current account on page load',
+    (tester) async {
+      mockPresenter();
+      await loadPage(tester);
+
+      verify(
+        () => presenter.loadCurrentAccount(),
+      ).called(1);
+    },
+  );
 }
 
-class SplashPage extends StatelessWidget {
-  const SplashPage({super.key});
+abstract class SplashPresenter {
+  Future<void> loadCurrentAccount();
+}
+
+class SplashPage extends StatefulWidget {
+  final SplashPresenter presenter;
+
+  const SplashPage({
+    Key? key,
+    required this.presenter,
+  }) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    widget.presenter.loadCurrentAccount();
+  }
 
   @override
   Widget build(BuildContext context) {
