61b2d1620c90d5bd0527721d97115b19d6b9af97
amandadetofol <amandadetofol@MacBook-Pro-de-Amanda.local>
2024-11-11 08:36:52 -0300

    creating add account usecase

---
+++ flutter_course/lib/lib/domain/usecases/add_account.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+
+import '../entities/entities.dart';
+
+abstract class Authentication {
+  Future<AccountEntity>? auth({
+    required AddAccountParams parameters,
+  });
+}
+
+class AddAccountParams extends Equatable {
+  final String name;
+  final String email;
+  final String password;
+  final String passwordConfirmation;
+
+  const AddAccountParams({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.passwordConfirmation,
+  });
+
+  @override
+  List<Object?> get props => [
+        name,
+        email,
+        password,
+        passwordConfirmation,
+      ];
+}
